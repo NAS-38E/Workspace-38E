13e6abee02e5d14ed36daf05de06dfe12167a034
NAS3 <NAS-38E@users.noreply.github.com>
2023-04-18 19:16:46 +0200

    Laboratorio1

--- factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(num):
-    print("Valor inicial ->",num)
-    if num > 1:
-        num = num * factorial(num - 1)
-    print("Valor final ->", num)
-    return num
-
-print(factorial(5))
